3863bcfa1e56cd55ec66543debaf01d5f0e8c72c
unknown <DIEGO@DIEGO-DESKTOP.(none)>
2012-09-22 00:55:24 -0300

    t

---
+++ PLBSoft/src/modelo/TipoTanque.java
@@ -0,0 +1,93 @@
+package modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Table(name="TIPO_TANQUE")
+@Entity
+
+public class TipoTanque implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 60227753724977383L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column 
+	private String revestimento;
+	
+	@Column
+	private Boolean status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRevestimento() {
+		return revestimento;
+	}
+
+	public void setRevestimento(String revestimento) {
+		this.revestimento = revestimento;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TipoTanque other = (TipoTanque) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
