ef61a0b0a4b7772b3ca86dc907fa01e25538a596
unknown <DIEGO@DIEGO-DESKTOP.(none)>
2012-09-22 01:00:32 -0300

    t1

--- PLBSoft/src/modelo/Especie.java
+++ PLBSoft/src/modelo/Especie.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Especie {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY);
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column
