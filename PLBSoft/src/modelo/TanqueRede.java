e338cb1c39756ac4ccf50c67c80dae16ac98b1b5
unknown <Administrador@diego-netbook.(none)>
2012-09-28 00:14:21 -0300

    alteracao dos modelos

--- PLBSoft/src/modelo/TanqueRede.java
+++ PLBSoft/src/modelo/TanqueRede.java
@@ -3,13 +3,11 @@ package modelo;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -34,8 +32,7 @@ public class TanqueRede implements Serializable {
 	@Column
 	private Boolean status;
 	
-	@ManyToOne(cascade=CascadeType.ALL) 
-	@JoinColumn(name="id") 
+	@ManyToOne 
 	private Tanque tanque; 
 
 	public Long getId() {
